b0576cbcd8458f1eed7f81b424ab3e8fe8f23d16
eli <elizabeth.johana.gonzalez@gmail.com>
2021-06-17 13:51:42 -0300

    correct bug

--- fit_MICE_profiles.py
+++ fit_MICE_profiles.py
@@ -8,22 +8,31 @@ from fit_profiles_curvefit import *
 from multiprocessing import Pool
 from multiprocessing import Process
 
+zhalos = fits.open('../catalogs/halo_props/out_8_2_plus.fits')[1].data.z_v
+profiles = np.loadtxt('../catalogs/halo_props/out_profile_8_2.bz2',skiprows=1,delimiter=',')
+ncores = 32
+
+
 def fit_profile(pro,z):
     
          r = np.arange(16)*(pro[1]/15.)  
          r = (r[:-1] + np.diff(r)/2.)/1000
          
-         rho   = pro[2:17]
-         rho_E = pro[17:32]
-         S     = pro[32:47]
-         S_E   = pro[47:]
+         mr = 0.7*pro[1]
          
+         rho   = pro[2:17][mr]
+         rho_E = pro[17:32][mr]
+         S     = pro[32:47][mr]
+         S_E   = pro[47:][mr]
+
+        r      = r[mr]
+        
          erho = np.ones(15)*50.
          
-         rho_f    = rho_fit(r,rho/(1.e6**3),erho,z,cosmo,True)
-         rho_E_f    = rho_fit(r,rho_E/(1.e6**3),erho,z,cosmo,True)
-         S_f      = Sigma_fit(r,S/(1.e6**2),erho,z,cosmo,True)
-         S_E_f      = Sigma_fit(r,S_E/(1.e6**2),erho,z,cosmo,True)
+         rho_f    = rho_fit(r[rho>0],rho[rho>0]/(1.e6**3),erho[rho>0],z,cosmo,True)
+         rho_E_f    = rho_fit(r[rho_E>0],rho_E[rho_E>0]/(1.e6**3),erho[rho_E>0],z,cosmo,True)
+         S_f      = Sigma_fit(r[S>0],S[S>0]/(1.e6**2),erho[S>0],z,cosmo,True)
+         S_E_f      = Sigma_fit(r[S_E>0],S_E[S_E>0]/(1.e6**2),erho[S_E>0],z,cosmo,True)
          
          return [np.log10(rho_f.M200),rho_f.c200,
                  np.log10(rho_E_f.M200),rho_E_f.c200,
@@ -31,24 +40,17 @@ def fit_profile(pro,z):
                  np.log10(S_E_f.M200),S_E_f.c200]
                  
 
-def run_fit_profile(profiles,zhalos):
+def run_fit_profile(index):
     
-    output_fits = np.zeros((len(zhalos),8))
+    output_fits = np.zeros((len(index),8))
     
-    for j in range(len(zhalos)):
+    for j in index:
         print(j)
         output_fits[j] = fit_profile(profiles[j],zhalos[j])
         
 
-def run_fit_profile_unpack(minput):
-	return run_fit_profile(*minput)
 
-
-zhalos = fits.open('../catalogs/halo_props/out_8_2_plus.fits')[1].data.z_v
-profiles = np.loadtxt('../catalogs/halo_props/out_profile_8_2.bz2',skiprows=1,delimiter=',')
-ncores = 32
-
-index = np.arange(len(cat))
+index = np.arange(len(zhalos))
 
 slicer = int(round(len(index)/float(ncores), 0))
 slices = ((np.arange(ncores-1)+1)*slicer).astype(int)
@@ -56,10 +58,6 @@ slices = slices[(slices <= len(index))]
 
 index_splitted = np.split(index,slices)
 
-files = np.arange(ncores)
-
-entrada = np.array([index_splitted,files,pesos]).T
-
 pool = Pool(processes=(ncores))
-pool.map(axis_neigh_unpack, entrada)
+salida = pool.map(run_fit_profile, index_splitted)
 pool.terminate()
