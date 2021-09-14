c7c140c876ca15492dc51386115a00cd7bc3330f
elizabethjg <elizabet.johana.gonzalez@gmail.com>
2021-09-14 18:11:41 -0300

    fit with colossus - debug

--- fit_models.py
+++ fit_models.py
@@ -21,26 +21,37 @@ class Sigma_fit:
         xplot   = np.arange(0.001,R.max()+1.,0.001)
 
         p = profile_nfw.NFWProfile(M = 1.e13, c = 4., z = z, mdef = '200c')
+
+        try:
         
-        out = p.fit(R*1000., Sigma/(1.e3**2), 'Sigma', q_err = err/(1.e3**2))
+            out = p.fit(R*1000., Sigma/(1.e3**2), 'Sigma', q_err = err/(1.e3**2))
+            
+            rhos,rs = out['x']
+            
+            prof = profile_nfw.NFWProfile(rhos = rhos, rs = rs)
+            
+            ajuste = prof.surfaceDensity(R*1000.)*(1.e3**2)
+            yplot  = prof.surfaceDensity(xplot*1000.)*(1.e3**2)
+            
+            BIN= len(Sigma)
+            
+            res=np.sqrt(((((np.log10(ajuste)-np.log10(Sigma))**2)).sum())/float(BIN-2))
+
+            M200 = prof.MDelta(z,'200c')
+            c200 = prof.RDelta(z,'200c')/rs
         
-        rhos,rs = out['x']
-        
-        prof = profile_nfw.NFWProfile(rhos = rhos, rs = rs)
-        
-        ajuste = prof.surfaceDensity(R*1000.)*(1.e3**2)
-        yplot  = prof.surfaceDensity(xplot*1000.)*(1.e3**2)
-        
-        BIN= len(Sigma)
-        
-        res=np.sqrt(((((np.log10(ajuste)-np.log10(Sigma))**2)).sum())/float(BIN-2))
+        except:
+            yplot = xplot
+            res   = -999.
+            M200  = -999.
+            c200  = -999.
         
 
         self.xplot = xplot
         self.yplot = yplot
         self.res  = res
-        self.M200 = prof.MDelta(z,'200c')
-        self.c200 = prof.RDelta(z,'200c')/rs
+        self.M200 = M200
+        self.c200 = c200
 
 class rho_fit:
 	# R en Mpc, rho M_Sun/Mpc3
@@ -52,23 +63,33 @@ class rho_fit:
         xplot   = np.arange(0.001,R.max()+1.,0.001)
 
         p = profile_nfw.NFWProfile(M = 1.e13, c = 4., z = z, mdef = '200c')
+
+        try:
         
-        out = p.fit(R*1000., rho/(1.e3**3), 'rho', q_err = err/(1.e3**3))
+            out = p.fit(R*1000., rho/(1.e3**3), 'rho', q_err = err/(1.e3**3))
+            rhos,rs = out['x']
+            
+            prof = profile_nfw.NFWProfile(rhos = rhos, rs = rs)
+            
+            ajuste = prof.surfaceDensity(R*1000.)*(1.e3**2)
+            yplot  = prof.surfaceDensity(xplot*1000.)*(1.e3**2)
+            
+            BIN= len(rho)
+            
+            res=np.sqrt(((((np.log10(ajuste)-np.log10(rho))**2)).sum())/float(BIN-2))
+            
+            M200 = prof.MDelta(z,'200c')
+            c200 = prof.RDelta(z,'200c')/rs
         
-        rhos,rs = out['x']
-        
-        prof = profile_nfw.NFWProfile(rhos = rhos, rs = rs)
-        
-        ajuste = prof.surfaceDensity(R*1000.)*(1.e3**2)
-        yplot  = prof.surfaceDensity(xplot*1000.)*(1.e3**2)
-        
-        BIN= len(rho)
-        
-        res=np.sqrt(((((np.log10(ajuste)-np.log10(rho))**2)).sum())/float(BIN-2))
+        except:
+            yplot = xplot
+            res   = -999.
+            M200  = -999.
+            c200  = -999.
         
 
         self.xplot = xplot
         self.yplot = yplot
         self.res  = res
-        self.M200 = prof.MDelta(z,'200c')
-        self.c200 = prof.RDelta(z,'200c')/rs
+        self.M200 = M200
+        self.c200 = c200
