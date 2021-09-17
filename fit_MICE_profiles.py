03848da5c9b40436de0c3117d06d61286ca0ab9e
elizabethjg <elizabet.johana.gonzalez@gmail.com>
2021-09-17 10:12:47 -0300

    test time

--- fit_MICE_profiles.py
+++ fit_MICE_profiles.py
@@ -27,8 +27,12 @@ main = main0.loc[rind]
 profiles = profiles0[rind]
 
 nrings = 25
-rhopro = profiles[:,2+nrings:2+2*nrings]
-m = np.sum(rhopro>0,axis=1) > 4
+rho   = profiles[:,2+nrings:2+2*nrings]
+rho_E = profiles[:,2+2*nrings:2+3*nrings]
+S     = profiles[:,2+3*nrings:2+4*nrings]
+S_E   = profiles[:,2+4*nrings:]
+
+m = (np.sum(rho>0,axis=1) > 4)*(np.sum(rho_E>0,axis=1) > 4)*(np.sum(S>0,axis=1) > 4)*(np.sum(S_E>0,axis=1) > 4)
 
 
 index = np.arange(len(profiles))[m]
@@ -212,7 +216,7 @@ hn = main['column_halo_id']
 
 output = np.column_stack((hn,output_all))
     
-out_file = '/home/elizabeth/halo_props2/lightconedir_129/halo_props2_'+part+'_mass_sample_random.csv.bz2'
+out_file = '/home/elizabeth/halo_props2/lightconedir_129/halo_props2_'+part+'_mass_test.csv.bz2'
 
 head = 'column_halo_id,lgMDelta,Delta,lgMNFW_rho,cNFW_rho,resNFW_rho,nb_rho,lgMNFW_rho_E,cNFW_rho_E,resNFW_rho_E,nb_rho_E,lgMNFW_S,cNFW_S,resNFW_S,nb_S,lgMNFW_S_E,cNFW_S_E,resNFW_S_E,nb_S_E,lgMEin_rho,cEin_rho,alpha_rho,resEin_rho,lgMEin_rho_E,cEin_rho_E,alpha_rho_E,resEin_E,lgMEin_S,cEin_S,alpha_S,resEin_S,lgMEin_S_E,cEin_S_E,alpha_S_E,resEin_S_E'
 
