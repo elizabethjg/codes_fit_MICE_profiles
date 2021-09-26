619d35d65c99e4227487483bfb1b52ec584b9590
elizabethjg <elizabet.johana.gonzalez@gmail.com>
2021-09-26 10:43:20 -0300

    run for hm - rlim

--- fit_MICE_profiles.py
+++ fit_MICE_profiles.py
@@ -78,10 +78,10 @@ def fit_profile(pro,z,plot=False):
          MDelta = Msum[j200]
          Delta  = ((Msum/Vsum)/roc_mpc)[j200]
          
-         mrho = (rho > 0.)#*(r > 0.15)
-         mS = (S > 0.)#*(r > 0.15)
-         mrhoe = (rho_E > 0.)#*(r > 0.15)
-         mSe = (S_E > 0.)#*(r > 0.15)
+         mrho = (rho > 0.)*(r > 0.15)
+         mS = (S > 0.)*(r > 0.15)
+         mrhoe = (rho_E > 0.)*(r > 0.15)
+         mSe = (S_E > 0.)*(r > 0.15)
          
          
          if mrho.sum() > 4. and mS.sum() > 4. and mrhoe.sum() > 4. and mSe.sum() > 4.:
@@ -212,7 +212,7 @@ hn = main['column_halo_id']
 
 output = np.column_stack((hn,output_all))
     
-out_file = '/home/elizabeth/halo_props2/lightconedir_129/halo_props2_'+part+'_mass_hM.csv.bz2'
+out_file = '/home/elizabeth/halo_props2/lightconedir_129/halo_props2_'+part+'_mass_hM_rlim.csv.bz2'
 
 head = 'column_halo_id,lgMDelta,Delta,lgMNFW_rho,cNFW_rho,resNFW_rho,nb_rho,lgMNFW_rho_E,cNFW_rho_E,resNFW_rho_E,nb_rho_E,lgMNFW_S,cNFW_S,resNFW_S,nb_S,lgMNFW_S_E,cNFW_S_E,resNFW_S_E,nb_S_E,lgMEin_rho,cEin_rho,alpha_rho,resEin_rho,lgMEin_rho_E,cEin_rho_E,alpha_rho_E,resEin_E,lgMEin_S,cEin_S,alpha_S,resEin_S,lgMEin_S_E,cEin_S_E,alpha_S_E,resEin_S_E'
 
