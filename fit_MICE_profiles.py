c8e158db7936b1369606b2171927b8e33a37bd81
elizabethjg <elizabet.johana.gonzalez@gmail.com>
2022-03-29 08:46:34 -0300

    fit 2

--- fit_MICE_profiles.py
+++ fit_MICE_profiles.py
@@ -18,7 +18,7 @@ part = args.file
 t0 = time()
 cosmo = LambdaCDM(H0=100, Om0=0.25, Ode0=0.75)
 
-ncores   = 32
+ncores   = 56
 main     = pd.read_csv('/home/elizabeth/halo_props2/lightconedir_129/halo_props2_'+part+'_main.csv.bz2')
 profiles = np.loadtxt('/home/elizabeth/halo_props2/lightconedir_129/halo_props2_'+part+'_pro.csv.bz2',skiprows=1,delimiter=',')
 
